7aa1fa96251d9462de919be11951bd9d3fca351c
tehwalk <eiraloupi2@gmail.com>
2023-12-09 02:40:22 +0200

    2nd commit
    -water physics test

--- Scripts/Draggable.cs
+++ Scripts/Draggable.cs
@@ -1,15 +1,39 @@
 using Godot;
 using System;
 
-public partial class Draggable : Node
+public partial class Draggable : StaticBody2D
 {
+	bool isDraggable = false;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if(Input.IsMouseButtonPressed(MouseButton.Left) && isDraggable)
+		{
+			this.Position = GetNode<Camera2D>("Camera2D").GetViewport().GetMousePosition();
+		}
 	}
+
+
+	public void OnMouseEntered()
+	{
+		isDraggable = true;
+	}
+
+	public void OnMouseExited()
+	{
+		isDraggable = false;
+	}
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+    }
+
+
 }
