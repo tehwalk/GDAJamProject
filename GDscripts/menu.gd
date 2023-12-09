482052b00ec0d9bd46be60b96a08662ef5542a44
SimonianMossi <92452093+SimonianMossi@users.noreply.github.com>
2023-12-09 16:15:03 +0200

    boobs

--- GDscripts/menu.gd
+++ GDscripts/menu.gd
@@ -13,10 +13,30 @@ func _process(delta):
 
 func _on_exit_pressed():
 	$Panel.visible=false
+	$PanelShadow.visible=false
+	
+	$"Start Shadow".visible=true
+	$"Credits Shadow".visible=true
+	$"Title Shadow".visible=true
+	$Title.visible=true
+	$ColorRect2.visible=true
+	$ColorRect3.visible=true
+	$Start.visible=true
+	$Credits.visible=true
 
 
 func _on_credits_pressed():
 	$Panel.visible=true
+	$PanelShadow.visible=true
+	
+	$"Start Shadow".visible=false
+	$"Credits Shadow".visible=false
+	$"Title Shadow".visible=false
+	$Title.visible=false
+	$ColorRect2.visible=false
+	$ColorRect3.visible=false
+	$Start.visible=false
+	$Credits.visible=false
 
 
 func _on_start_pressed():
